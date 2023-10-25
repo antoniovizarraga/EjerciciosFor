a082e438142b738bb31c1c9865af7999ae336c76
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2023-10-25 09:51:50 +0200

    Ejercicio 6 terminado

---
+++ src/ejerciciosfor/Ejercicio6.java
@@ -0,0 +1,15 @@
+package ejerciciosfor;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		int n = 0;
+		for(int i = 1; i < 20; i += 2)
+			n += i;
+
+		System.out.println(n);
+	}
+	
+	
+
+}
